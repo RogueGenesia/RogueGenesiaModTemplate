16d73abee17edcb7489ae63bb6ee5852a32f58a4
Stefan Haacker <code@haacker.me>
2022-10-19 09:44:53 -0400

    Adding helpers and some required DLLs

---
+++ Helpers/SpriteHelper.cs
@@ -0,0 +1,35 @@
+using System.IO;
+using UnityEngine;
+
+namespace RogueGenesiaModTemplate.Helpers;
+
+public static class SpriteHelper
+{
+    private static Texture2D LoadPNGIntoTexture(string filePath) {
+        Texture2D tex = null;
+
+        if (File.Exists(filePath)) {
+            var fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(2, 2, TextureFormat.RGBA32, true, false);
+            ImageConversion.LoadImage(tex, fileData);
+            
+            if (tex != null)
+            {
+                // Set FilterMode so the images don't end up blurry
+                tex.filterMode = FilterMode.Point;
+            }
+            else
+            {
+                Plugin.Log.LogInfo($"Texture couldn't be loaded: {filePath}");
+            }
+        }
+
+        return tex;
+    }
+
+    public static Sprite LoadSpriteFromFile(string filePath)
+    {
+        var tex = LoadPNGIntoTexture(filePath);
+        return Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);
+    }
+}
